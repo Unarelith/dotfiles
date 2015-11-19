d3f1d284c2997dc406aff05eb3a799da11e6e1c2
Quentin Bazin <quent42340@gmail.com>
2015-11-19 11:41:43 +0100

    Updated.

--- link/.local/bin/prepare_my_repo.sh
+++ link/.local/bin/prepare_my_repo.sh
@@ -1,8 +1,10 @@
+#/bin/bash
 blih repository create $1 && \
 blih repository setacl $1 ramassage-tek r && \
 cp -r ~/rendu/.default $1 && \
 cd $1 && \
 git init . && \
-git remote add origin git@git.epitech.eu:/bazin_q/$1 && \
-git remote add gogs gogs@git.gnidmoo.tk:gnidmoo/$1.git && \
+git remote add origin git@git.epitech.eu:/${USER}/$(basename $(pwd)) && \
+git remote set-url --add --push origin git@git.epitech.eu:/${USER}/$(basename $(pwd)) && \
+git remote set-url --add --push origin gogs@git.gnidmoo.tk:gnidmoo/$(echo $(basename $(pwd)) | tr '[:upper:]' '[:lower:]').git && \
 blih repository getacl $1
