ca9ec2894ee69c10cc785c6287dd63e53a99b7db
Quentin Bazin <quent42340@gmail.com>
2015-11-20 18:08:26 +0100

    [update-gogs-repos] Colors added.

--- link/.local/bin/prepare_my_repo.sh
+++ link/.local/bin/prepare_my_repo.sh
@@ -5,6 +5,4 @@ cp -r ~/rendu/.default $1 && \
 cd $1 && \
 git init . && \
 git remote add origin git@git.epitech.eu:/${USER}/$(basename $(pwd)) && \
-git remote set-url --add --push origin git@git.epitech.eu:/${USER}/$(basename $(pwd)) && \
-git remote set-url --add --push origin gogs@git.gnidmoo.tk:gnidmoo/$(echo $(basename $(pwd)) | tr '[:upper:]' '[:lower:]').git && \
 blih repository getacl $1
