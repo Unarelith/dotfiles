ecbb76ec1eda0f531607a86a6a59587822e63152
Quentin Bazin <quent42340@gmail.com>
2015-11-24 18:01:12 +0100

    [blim] Overlay of blih: added. [prepare_my_repo] Improved a lot. [update-gogs-repos] Little bug with colors fixed.

--- link/.local/bin/prepare_my_repo.sh
+++ link/.local/bin/prepare_my_repo.sh
@@ -1,8 +1,41 @@
 #/bin/bash
-blih repository create $1 && \
-blih repository setacl $1 ramassage-tek r && \
-cp -r ~/rendu/.default $1 && \
-cd $1 && \
-git init . && \
-git remote add origin git@git.epitech.eu:/${USER}/$(basename $(pwd)) && \
-blih repository getacl $1
+if [ ! -e "$1" ]
+then
+	tries_left=3
+	blih_try=1
+	while [ $tries_left -gt 0 ] && [ $blih_try -eq 1 ]
+	do
+		echo -n "Password: "
+		read -s unix_passwd ; echo
+		
+		blih_try=$(echo "$unix_passwd" | blim whoami > /dev/null 2>&1 ; echo $?)
+		
+		let tries_left--
+	done
+	
+	if [ $blih_try -eq 1 ]
+	then
+		echo "Failed: Wrong password."
+		exit 1
+	fi
+	
+	echo "$unix_passwd" | blim repository create $1
+	echo "$unix_passwd" | blim repository setacl $1 ramassage-tek r
+	echo "$unix_passwd" | blim repository getacl $1
+	
+	if [ "$?" == "0" ] && [ "$(git clone "git@git.epitech.eu:/${USER}/$1" > /dev/null 2>&1 ; echo "$?")" == "0" ]
+	then
+		cd "$1"
+		
+		if [ -e ~/rendu/.default/.gitignore ]
+		then
+			cp ~/rendu/.default/.gitignore .
+		fi
+		
+		echo "Done."
+	else
+		echo "Error: 'git clone' failed."
+	fi
+else
+	echo "Error: Folder '$1' already exists."
+fi
