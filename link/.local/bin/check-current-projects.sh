ba00cbd330ae66fe7e37eac06116cff62f069d26
Quentin Bazin <quent42340@gmail.com>
2015-12-31 19:24:53 +0100

    [check-current-projects.sh] Improved. [GnuCash] Reports added.

--- link/.local/bin/check-current-projects.sh
+++ link/.local/bin/check-current-projects.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-cd /home/bazin_q/rendu
+base_dir="/home/bazin_q/rendu"
 
-for project in $(cat current)
+for project in $(cat "$base_dir/current")
 do
-	cd $project
+	cd "$base_dir"
+	
+	if [ ! -d "$project" ]
+	then
+		notify-send -u critical -t 5000 "$(pwd)/$project" "Folder not found"
+		continue
+	fi
+	
+	cd "$project"
 	
 	norm_errors=$(python2 /home/bazin_q/.local/bin/gistfile1.py $(find . -name '*.c' -or -name '*.h' -or -name 'Makefile' | grep -v "tests-") | tail -1 | awk '{print $2*-1}');
 	if [ $norm_errors -gt 0 ]
 	then
 		notify-send -u critical -t 10000 "$(basename $(pwd))" "You have $norm_errors norm errors."
+	else
+		notify-send -u normal -t 2000 "$(basename $(pwd))" "You have $norm_errors norm errors."
 	fi
-	
-	cd ..
 done
