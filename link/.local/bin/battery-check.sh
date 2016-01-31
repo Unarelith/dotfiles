7321f272a4a65ee120a345e98d52bda03165b60b
Quentin Bazin <quent42340@gmail.com>
2016-01-31 21:13:09 +0100

    [epinet|battery-check.sh] Improved.

--- link/.local/bin/battery-check.sh
+++ link/.local/bin/battery-check.sh
@@ -2,28 +2,23 @@
 
 # Battery level warning script
 
-if [ -z "$(acpi -b | grep Discharging)" ]
-then
-	exit 0
-fi
+[ -z "$(acpi -b | grep Discharging)" ] && exit 0
 
-battery_level=`acpi -b | grep -oP '[0-9]+(?=%)'`
+battery_level=$(acpi -b | grep -oP '[0-9]+(?=%)')
 
 flags_low="-u normal -t 5000"
-flags_very_low="-u critical -t 8000"
-flags_critical="-u critical -t 10000"
+flags_very_low="-u critical -t 80000"
+flags_critical="-u critical -t 100000"
 
-if [[ $battery_level -le 20 && $battery_level -gt 10 ]]
+if [ $battery_level -le 20 ] && [ $battery_level -gt 10 ]
 then
 	notify-send ${flags_low} "Battery low warning"  "Battery level: ${battery_level}%"
-elif [[ $battery_level -le 10 && $battery_level -gt 5 ]]
+elif [ $battery_level -le 10 ] && [ $battery_level -gt 5 ]
 then
-	notify-send ${flags_very_low} "Battery very low warning" \
-	"Battery level: ${battery_level}%
-	Use ac power now,
-	or shutdown is imminent - close applications"
-elif [[ $battery_level -le 5 ]]
+	notify-send ${flags_very_low} "Battery very low warning" "Battery level: ${battery_level}%"
+elif [ $battery_level -le 5 ]
 then
-	notify-send ${flags_critical} "Battery critical warning" \
-	"Battery level: ${battery_level}%"
+	notify-send ${flags_critical} "Battery critical warning" "Battery level: ${battery_level}%\nSleeping in 10 seconds."
+	sleep 10
+	[ -n "$(acpi -b | grep Discharging)" ] && systemctl suspend
 fi
--- link/.local/bin/check-current-projects.sh
+++ link/.local/bin/check-current-projects.sh
@@ -2,6 +2,8 @@
 
 base_dir="/home/${USER}/rendu"
 
+notify-send -u normal -t 5000 "check-current-projects.sh" "Mode -nocheat enabled for goku."
+
 for project in $(cat "$base_dir/current")
 do
 	cd "$base_dir"
@@ -14,7 +16,7 @@ do
 	
 	cd "$project"
 	
-	norm_errors=$(python2 /home/${USER}/.local/bin/gistfile1.py $(find . -name '*.c' -or -name '*.h' -or -name 'Makefile' | grep -v "tests-") | tail -1 | awk '{print $2*-1}');
+	norm_errors=$(python2 /home/${USER}/.local/bin/gistfile1.py -nocheat $(find . -name '*.c' -or -name '*.h' -or -name 'Makefile' | grep -v "tests-") | tail -1 | awk '{print $2*-1}');
 	if [ $norm_errors -gt 0 ]
 	then
 		notify-send -u critical -t 10000 "$(basename $(pwd))" "You have $norm_errors norm errors."
