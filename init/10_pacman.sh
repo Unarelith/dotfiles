5c0131c7341975fa935daa5fd74e79df3a40cd2e
Quentin Bazin <quent42340@gmail.com>
2016-09-30 10:01:40 +0200

    [i3 scripts: now_playing] Updated.

--- init/10_pacman.sh
+++ init/10_pacman.sh
@@ -1,4 +1,4 @@
-defaultpackages='
+default_packages='
 chromium
 cronie
 dash
@@ -9,7 +9,9 @@ i3-wm
 i3lock
 i3status
 kdegraphics-kolourpaint
+lm_sensors
 lxappearance
+mlocate
 nautilus
 nautilus-open-terminal
 network-manager-applet
@@ -24,14 +26,26 @@ python2
 python3
 redshift
 rxvt-unicode
+smplayer
+smtube
 synapse
+sysstat
 ttf-dejavu
 xcompmgr
 xsel'
 
-defaultaurpackages='volnoti dropbox nautilus-dropbox'
+default_aur_packages='
+dropbox
+i3blocks
+nautilus-dropbox
+volnoti'
 
-systemdservices='NetworkManager cronie'
+systemd_services='
+cronie
+NetworkManager'
+
+systemd_user_services='
+pulseaudio'
 
 echo -ne "$BOLD:: Do you want to install yaourt? [y/N]" $WHITE
 
@@ -44,7 +58,7 @@ then
 	cd package-query && \
 	makepkg -si && \
 	cd .. && rm -rf package-query package-query.tar.gz
-	
+
 	curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz && \
 	tar xvzf yaourt.tar.gz && \
 	cd yaourt && \
@@ -54,28 +68,34 @@ fi
 
 echo
 
-echo -e "${BCYAN}* Default packages: $WHITE$defaultpackages\n"
-echo -e "${BCYAN}* Default AUR packages: $WHITE$defaultaurpackages\n"
+echo -e "${BCYAN}* Default packages: $WHITE$default_packages\n"
+echo -e "${BCYAN}* Default AUR packages: $WHITE$default_aur_packages\n"
 echo -ne "${BOLD}:: Do you want to install them? [y/N]" $WHITE
 
 read answer
 
 if [ "$answer" == "y" ] || [ "$anwser" == "Y" ]
 then
-	yaourt -S $defaultpackages $defaultaurpackages
+	yaourt -S $default_packages $default_aur_packages
 fi
 
 echo
 
-echo -e "${BOLD}Systemd services: $WHITE$systemdservices\n"
+echo -e "${BOLD}Systemd services: $WHITE$systemd_services\n"
+echo -e "${BOLD}Systemd user services: $WHITE$systemd_user_services\n"
 echo -ne "${BOLD}:: Do you want to enable them? [y/N]" $WHITE
 
 read answer
 
 if [ "$answer" == "y" ] || [ "$anwser" == "Y" ]
 then
-	for service in $systemdservices
+	for service in $systemd_services
 	do
 		sudo systemctl enable $service
+	done
+
+	for service in $systemd_user_services
+	do
+		sudo systemctl --user enable $service
 	done
 fi
