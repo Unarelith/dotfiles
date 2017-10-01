dfa27fead757f1f69f129eca315f472ffa36c652
Quentin Bazin <quent42340@gmail.com>
2017-10-01 15:44:59 +0200

    Updated.

--- init/30_vim.sh
+++ init/30_vim.sh
@@ -2,15 +2,19 @@
 if [[ "$(type -P vim)" ]]; then
 	mkdir ~/.vim/bundle
 	cd ~/.vim/bundle
-	
+
 	git clone git://github.com/gmarik/Vundle.vim
-	
+
 	vim +BundleUpdate
+
+	# Install YouCompleteMe
+	cd ~/.vim/bundle/YouCompleteMe
+	./install.py --clang-completer --system-libclang
 fi
 
 # Download NeoVim plugins.
 if [[ "$(type -P nvim)" ]]; then
 	curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	
+
 	nvim +PlugInstall
 fi
